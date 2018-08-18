940c223201ed27380f44b33a7deac0cf01b6233a
homerooliveira <homero.santos.001@acad.pucrs.br>
2018-08-18 15:27:30 -0300

    moved code to struct called Queue

--- THESimulatorPlayground.playground/Contents.swift
+++ THESimulatorPlayground.playground/Contents.swift
@@ -15,7 +15,7 @@ var count: Int = 0
 var randomNumbers: [Double] = [0.1195, 0.3491, 0.9832, 0.7731, 0.8935, 0.2103, 0.0392, 0.1782]
 
 func random() -> Double {
-//    return Double(arc4random()) / Double(UInt32.max)
+    return Double(arc4random()) / Double(UInt32.max)
     defer {
        count += 1
     }
@@ -26,70 +26,71 @@ func conversion(lowerBound: Double, upperBound: Double) -> Double {
     return (upperBound - lowerBound) * random() + lowerBound
 }
 
-var agenda: [Event] = [Event(type: .arrival, time: 2.5)]
-var sizeOfQueue: Int = 0
-var states: [Double] = Array(repeating: 0, count: 5)
-var time: Double = 0
+struct Queue {
+    
+    var agenda: [Event] = [Event(type: .arrival, time: 2.5)]
+    var sizeOfQueue: Int = 0
+    var states: [Double] = Array(repeating: 0, count: 5)
+    var time: Double = 0
+    var currentEvent: Event?
 
-func accountForProbabilities() {
-    guard let event = agenda.filter({ $0.time > time })
-        .min(by: { $0.time < $1.time }) else {
+    
+    mutating func accountForProbabilities() {
+    guard let event = currentEvent else {
                     return
     }
-    
     states[sizeOfQueue] = event.time - time + states[sizeOfQueue]
-    
     time = event.time
-//    print("event: \(event.type)")
-//    print("state: \(states)")
-//    print("time: \(time)")
 }
 
-func scheduleExit(time: Double) {
-    agenda.append(Event(type: .exit, time: time))
+
+    mutating func schedule(for type: EventType, time: Double) {
+    agenda.append(Event(type: type, time: time))
 }
 
-func scheduleArrival(time: Double) {
-    agenda.append(Event(type: .arrival, time: time))
-}
-
-func executeArrival() {
+    mutating func executeArrival() {
     accountForProbabilities()
     if sizeOfQueue < 4 {
         sizeOfQueue += 1
         if sizeOfQueue <= 1 {
-            scheduleExit(time: time + conversion(lowerBound: 3, upperBound: 5))
+            schedule(for: .exit, time: time + conversion(lowerBound: 3, upperBound: 5))
         }
     }
-    scheduleArrival(time: time + conversion(lowerBound: 2, upperBound: 3))
+    schedule(for: .arrival, time: time + conversion(lowerBound: 2, upperBound: 3))
 }
 
-func executeExit() {
+    mutating func executeExit() {
     accountForProbabilities()
     sizeOfQueue -= 1
     if sizeOfQueue >= 1 {
-        scheduleExit(time: time + conversion(lowerBound: 3, upperBound: 5))
+        schedule(for: .exit, time: time + conversion(lowerBound: 3, upperBound: 5))
     }
 }
-
-for i in 0..<7 {
-    if let event = agenda.filter({ $0.time > time })
-        .min(by: { $0.time < $1.time }) {
-//        print("\(i) - \(event.type) - \(event.time) - currentTime: \(time)")
-        switch event.type {
-        case .arrival:
-            executeArrival()
-        case .exit:
-            executeExit()
+    mutating func execute(){
+        for i in 0..<1000 {
+            currentEvent = agenda.filter({ $0.time > time })
+                .min(by: { $0.time < $1.time })
+            if let event = currentEvent {
+                switch event.type {
+                case .arrival:
+                    executeArrival()
+                case .exit:
+                    executeExit()
+                }
+            }
+        }
+        states.enumerated().forEach { (index, state) in
+            print("state \(index): \((states[index] / time) * 100 )")
+        }
+        print("total: \(time)")
+        print()
+        agenda.forEach { (event) in
+            print("\(event.type) \(event.time)")
         }
     }
 }
 
-states.enumerated().forEach { (index, state) in
-    print("state \(index): \((states[index] / time) * 100 )")
-}
-print("total: \(time)")
-//print()
-//agenda.forEach { (event) in
-//    print("\(event.type) \(event.time)")
-//}
+var queue = Queue()
+queue.execute()
+
+
