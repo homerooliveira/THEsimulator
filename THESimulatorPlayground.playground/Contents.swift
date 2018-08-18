e4911313eca17c9350baefe06a338f33adb73488
homerooliveira <homero.santos.001@acad.pucrs.br>
2018-08-18 19:12:28 -0300

    added fields to queue

--- THESimulatorPlayground.playground/Contents.swift
+++ THESimulatorPlayground.playground/Contents.swift
@@ -10,6 +10,9 @@ struct Event {
     let type: EventType
     let time: Double
 }
+struct ExecutionInfo {
+    
+}
 
 var count: Int = 0
 var randomNumbers: [Double] = [0.1195, 0.3491, 0.9832, 0.7731, 0.8935, 0.2103, 0.0392, 0.1782]
@@ -17,80 +20,90 @@ var randomNumbers: [Double] = [0.1195, 0.3491, 0.9832, 0.7731, 0.8935, 0.2103, 0
 func random() -> Double {
     return Double(arc4random()) / Double(UInt32.max)
     defer {
-       count += 1
+        count += 1
     }
     return randomNumbers[count]
 }
 
-func conversion(lowerBound: Double, upperBound: Double) -> Double {
-    return (upperBound - lowerBound) * random() + lowerBound
+func conversion(_ range: ClosedRange<Double>) -> Double {
+    return (range.upperBound - range.lowerBound) * random() + range.lowerBound
 }
 
-struct Queue {
+class Queue {
     
     var agenda: [Event] = [Event(type: .arrival, time: 2.5)]
     var sizeOfQueue: Int = 0
-    var states: [Double] = Array(repeating: 0, count: 5)
+    var states: [Double]
     var time: Double = 0
-    var currentEvent: Event?
-
+    let numberOfStates: Int
+    let numberOfServer: Int
     
-    mutating func accountForProbabilities() {
-    guard let event = currentEvent else {
-                    return
+    init(numberOfStates: Int, numberOfServer: Int) {
+        self.numberOfStates = numberOfStates
+        self.numberOfServer = numberOfServer
+        states = Array(repeating: 0, count: numberOfStates + 1)
     }
-    states[sizeOfQueue] = event.time - time + states[sizeOfQueue]
-    time = event.time
-}
-
-
-    mutating func schedule(for type: EventType, time: Double) {
-    agenda.append(Event(type: type, time: time))
-}
-
-    mutating func executeArrival() {
-    accountForProbabilities()
-    if sizeOfQueue < 4 {
-        sizeOfQueue += 1
-        if sizeOfQueue <= 1 {
-            schedule(for: .exit, time: time + conversion(lowerBound: 3, upperBound: 5))
+    
+    func accountForProbabilities(event: Event) {
+        guard sizeOfQueue <= 4 else {
+            return
+        }
+        states[sizeOfQueue] = event.time - time + states[sizeOfQueue]
+        time = event.time
+    }
+    
+    
+    func schedule(for type: EventType, time: Double) {
+        agenda.append(Event(type: type, time: time))
+    }
+    
+    func executeArrival(event: Event) {
+        accountForProbabilities(event: event)
+        if sizeOfQueue < numberOfStates {
+            sizeOfQueue += 1
+            if sizeOfQueue <= numberOfServer {
+                schedule(for: .exit, time: time + conversion(3...5))
+            }
+        }
+        schedule(for: .arrival, time: time + conversion(2...3))
+    }
+    
+    func executeExit(event: Event) {
+        accountForProbabilities(event: event)
+        sizeOfQueue -= 1
+        if sizeOfQueue >= numberOfServer {
+            schedule(for: .exit, time: time + conversion(3...5))
         }
     }
-    schedule(for: .arrival, time: time + conversion(lowerBound: 2, upperBound: 3))
-}
-
-    mutating func executeExit() {
-    accountForProbabilities()
-    sizeOfQueue -= 1
-    if sizeOfQueue >= 1 {
-        schedule(for: .exit, time: time + conversion(lowerBound: 3, upperBound: 5))
-    }
-}
-    mutating func execute(){
-        for i in 0..<1000 {
-            currentEvent = agenda.filter({ $0.time > time })
-                .min(by: { $0.time < $1.time })
-            if let event = currentEvent {
+    
+    func execute() -> [[String]] {
+        for _ in 0..<100 {
+            if let event = agenda.filter({ $0.time > time })
+                .min(by: { $0.time < $1.time }) {
                 switch event.type {
                 case .arrival:
-                    executeArrival()
+                    executeArrival(event: event)
                 case .exit:
-                    executeExit()
+                    executeExit(event: event)
                 }
             }
         }
-        states.enumerated().forEach { (index, state) in
-            print("state \(index): \((states[index] / time) * 100 )")
+        
+        
+        var statesAsString = states.enumerated().map { (index, state) in
+            "state \(index)| \(state) | \((state / time) * 100 )%"
         }
-        print("total: \(time)")
-        print()
-        agenda.forEach { (event) in
-            print("\(event.type) \(event.time)")
+        statesAsString.append("total: \(time) | 100%")
+        
+        let agendaAsString = agenda.enumerated().map { (index, event) in
+            "\(index) - \(event.type) \(event.time)"
         }
+        
+        return [statesAsString, agendaAsString]
     }
 }
 
-var queue = Queue()
-queue.execute()
+var queue = Queue(numberOfStates: 4, numberOfServer: 1)
+print(queue.execute()[0])
 
 
